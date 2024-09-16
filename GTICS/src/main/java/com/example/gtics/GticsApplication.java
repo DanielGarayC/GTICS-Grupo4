d8c4f431a6b0b1b4db1e7d1b9059c39cbdd6cb60
Daniel <a20212591@pucp.edu.pe>
2024-09-15 23:08:53 -0500

    prueba

---
+++ GTICS/src/main/java/com/example/gtics/GticsApplication.java
@@ -0,0 +1,13 @@
+package com.example.gtics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GticsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GticsApplication.class, args);
+	}
+
+}
---
+++ GTICS/src/test/java/com/example/gtics/GticsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gtics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GticsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
