d838ef19a1179ba92d7f68afe00828ea87a4caad
Daniel <a20212591@pucp.edu.pe>
2024-09-19 02:01:20 -0500

    SuperAdminController  avance

---
+++ GTICS/src/main/java/com/example/gtics/controller/SuperAdminController.java
@@ -0,0 +1,43 @@
+package com.example.gtics.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class SuperAdminController {
+    @GetMapping("SuperAdmin/dashboard")
+    public String dashboard(){
+
+        return "SuperAdmin/Dashboard/dashboard-superadmin";
+    }
+    @GetMapping("SuperAdmin/listaAdminZonal")
+    public String listaGestionAdminZonal(){
+
+        return "SuperAdmin/GestionAdminZonal/admin-zonal-list";
+    }
+    @GetMapping("SuperAdmin/editarAdminZonal")
+    public String editarAdminZonal(){
+
+        return "SuperAdmin/GestionAdminZonal/admin-zonal-edit";
+    }
+    @GetMapping("SuperAdmin/crearAdminZonal")
+    public String crearAdminZonal(){
+
+        return "SuperAdmin/GestionAdminZonal/create-zonal-admin";
+    }
+    @GetMapping("SuperAdmin/listaAgente")
+    public String listaGestionAgente(){
+
+        return "SuperAdmin/GestionAgentes/agent-list";
+    }
+    @GetMapping("SuperAdmin/editarAgente")
+    public String editarAdminZonal(){
+
+        return "SuperAdmin/GestionAgentes/agent-edit";
+    }
+    @GetMapping("SuperAdmin/crearAgente")
+    public String crearAgente(){
+
+        return "SuperAdmin/GestionAgentes/create-agent";
+    }
+}
