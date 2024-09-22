a76525ca1dc2f3ddb09940dac8d359b94e05e391
AdrianLopez012 <142950326+AdrianLopez012@users.noreply.github.com>
2024-09-22 18:27:56 -0500

    q

--- GTICS/src/main/java/com/example/gtics/repository/UsuarioRepository.java
+++ GTICS/src/main/java/com/example/gtics/repository/UsuarioRepository.java
@@ -1,9 +1,14 @@
 package com.example.gtics.repository;
 
 import com.example.gtics.entity.Usuario;
+import jakarta.persistence.criteria.CriteriaBuilder;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface UsuarioRepository extends JpaRepository<Usuario,Integer> {
+    // Método personalizado para encontrar usuarios por rol
+    List<Usuario> findByIdRol_Id(Integer idRol);
 }
