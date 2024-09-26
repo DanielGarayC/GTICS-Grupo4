cb421552bf14c3d60160ebf73e4d7492e3c08f82
Brunocj <142907072+Brunocj@users.noreply.github.com>
2024-09-25 23:12:03 -0500

    Dashboard

--- GTICS/src/main/java/com/example/gtics/repository/OrdenRepository.java
+++ GTICS/src/main/java/com/example/gtics/repository/OrdenRepository.java
@@ -1,14 +1,24 @@
 package com.example.gtics.repository;
 
+import com.example.gtics.dto.FindCantOrdenesPorEstado;
+import com.example.gtics.dto.FindCantOrdenesPorMes;
 import com.example.gtics.entity.Orden;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
 public interface OrdenRepository extends JpaRepository<Orden, Integer> {
+  
+      @Query(value = "SELECT MONTH(o.fechaOrden) as mes, COUNT(o.idOrden) as totalOrdenes FROM Orden o\n" +
+            "GROUP BY MONTH(o.fechaOrden)", nativeQuery = true)
+    List<FindCantOrdenesPorMes> getOrdenesMes();
 
+    @Query(value="SELECT eo.nombreEstado, COUNT(o.idOrden) as totalOrdenes FROM Estadoorden eo\n" +
+            "JOIN Orden o ON eo.idEstadoOrden = o.idEstadoOrden GROUP BY eo.nombreEstado", nativeQuery = true)
+    List<FindCantOrdenesPorEstado> getOrdenesEstado();
 }
 
 
