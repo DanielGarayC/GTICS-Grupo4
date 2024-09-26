9a8fd43f03f1bedf4fb06b0ac497329280946da7
NadiaMHB <nhb_02@hotmail.com>
2024-09-26 18:13:14 -0500

    .

--- GTICS/src/main/java/com/example/gtics/repository/ProductoRepository.java
+++ GTICS/src/main/java/com/example/gtics/repository/ProductoRepository.java
@@ -11,12 +11,15 @@ import java.util.List;
 @Repository
 public interface ProductoRepository extends JpaRepository<Producto, Integer> {
 
+    // Consulta JPQL, referenciando la clase Producto
     @Query("SELECT p FROM Producto p WHERE p.borrado = 0")
     List<Producto> findAllActive();
 
-    @Query(value ="SELECT p.idProducto, p.nombreProducto, p.cantVentas FROM Producto p ORDER BY p.cantVentas DESC LIMIT 10", nativeQuery = true)
+    // Consulta nativa para obtener productos relevantes
+    @Query(value = "SELECT p.idProducto, p.nombreProducto, p.cantVentas FROM producto p ORDER BY p.cantVentas DESC LIMIT 10", nativeQuery = true)
     List<ProductoRelevanteDTO> findProductosRelevantes();
 
-    @Query(value = "SELECT SUM(cantVentas) AS totalVentas FROM Producto;", nativeQuery = true)
+    // Consulta nativa para obtener la cantidad total de ventas
+    @Query(value = "SELECT SUM(p.cantVentas) AS totalVentas FROM producto p", nativeQuery = true)
     CantidadProductos getCantidadProductos();
 }
