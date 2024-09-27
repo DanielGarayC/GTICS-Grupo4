400a6fb0ef7727f258c9bf20276ad7df88f6e7fb
RicardoEGFST <147290010+RicardoEGFST@users.noreply.github.com>
2024-09-27 17:22:44 -0500

    Update UsuarioRepository.java

--- GTICS/src/main/java/com/example/gtics/repository/UsuarioRepository.java
+++ GTICS/src/main/java/com/example/gtics/repository/UsuarioRepository.java
@@ -6,14 +6,15 @@ import com.example.gtics.dto.CantUsuariosBaneados;
 import com.example.gtics.dto.CantUsuariosRegistrados;
 import com.example.gtics.dto.CantidadAgentes;
 import com.example.gtics.entity.Usuario;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
+
 import java.util.List;
 
 @Repository
