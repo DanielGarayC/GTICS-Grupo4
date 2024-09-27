3b7440c16291ccc447673a980baf9e09967377ae
RicardoEGFST <147290010+RicardoEGFST@users.noreply.github.com>
2024-09-27 17:08:56 -0500

    Update UsuarioRepository.java

--- GTICS/src/main/java/com/example/gtics/repository/UsuarioRepository.java
+++ GTICS/src/main/java/com/example/gtics/repository/UsuarioRepository.java
@@ -12,7 +12,8 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
-
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import java.util.List;
 
 @Repository
