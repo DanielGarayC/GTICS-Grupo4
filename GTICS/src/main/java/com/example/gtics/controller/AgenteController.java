51a530fd55110024932ddcbd49590d94ba5898af
Jesus Alvarado <a20211688@pucp.edu.pe>
2024-09-29 17:57:20 -0500

    perfil agente con nav en todas las vistas

--- GTICS/src/main/java/com/example/gtics/controller/AgenteController.java
+++ GTICS/src/main/java/com/example/gtics/controller/AgenteController.java
@@ -6,11 +6,8 @@ import com.example.gtics.entity.*;
 import com.example.gtics.repository.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.ArrayList;
@@ -31,6 +28,18 @@ public class AgenteController {
     @Autowired
     EstadoOrdenRepository estadoOrdenRepository;
 
+
+    @ModelAttribute
+    public void addUsuarioToModel(Model model) {
+        Optional<Usuario> optUsuario = usuarioRepository.findById(13);
+        optUsuario.ifPresent(usuarioLogueado -> model.addAttribute("usuarioLogueado", usuarioLogueado));
+    }
+
+    @GetMapping({"/Agente/Perfil"})
+    public String pefil(){
+
+        return "Agente/miperfil";
+    }
 
 
     @GetMapping({"Agente"})
@@ -180,18 +189,6 @@ public class AgenteController {
         return "Agente/OrdenesDeUsuario/ordenesDeUsuario";
     }
 
-    @GetMapping({"Agente/Ordenes/Usuario/Lista"})
-    public String OrdenesUsuarioLista(@RequestParam("idUsuario") Integer idUsuario,Model model){
-        List<OrdenCarritoDto> ordenCarrito = ordenRepository.obtenerCarritoConDto(idUsuario);
-        Optional<Usuario> usr = usuarioRepository.findById(idUsuario);
-
-        if (ordenCarrito == null) {
-            ordenCarrito = new ArrayList<>();
-        }
-        model.addAttribute("ordenCarrito",ordenCarrito);
-        model.addAttribute("usuario",usr.get());
-        return "Agente/OrdenesDeUsuario/ordenesDeUsuarioLista";
-    }
 
 
     @PostMapping({"/Agente/Orden/editarOrden"})
