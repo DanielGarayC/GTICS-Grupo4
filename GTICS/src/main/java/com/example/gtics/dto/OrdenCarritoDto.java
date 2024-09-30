c4137c0329cc148373d02110b0a56586e55379d3
brandon1502 <brandon16in.m@gmail.com>
2024-09-30 13:27:00 -0500

    Falta mostrar correctamente la lista dsps de un filtrado

--- GTICS/src/main/java/com/example/gtics/dto/OrdenCarritoDto.java
+++ GTICS/src/main/java/com/example/gtics/dto/OrdenCarritoDto.java
@@ -3,6 +3,8 @@ package com.example.gtics.dto;
 import java.time.LocalDate;
 
 public interface OrdenCarritoDto{
+
+
     LocalDate getFechaOrden();
     double getMontoTotal();
     String getEstadoOrden();
--- GTICS/src/main/java/com/example/gtics/repository/OrdenRepository.java
+++ GTICS/src/main/java/com/example/gtics/repository/OrdenRepository.java
@@ -69,21 +69,22 @@ public interface OrdenRepository extends JpaRepository<Orden, Integer> {
     @Query(nativeQuery = true, value = "SELECT \n" +
             "    o.idOrden, \n" +
             "    o.fechaOrden, \n" +
-            "    SUM(p.precio) AS montoTotal, \n" +
+            "    SUM(phc.cantidadProducto * p.precio + COALESCE(o.costosAdicionales, 0.00)) + MAX(p.costoEnvio) AS montoTotal,  -- Cálculo del monto total con máximo costo de envío\n" +
             "    eo.nombreEstado AS estadoOrden, \n" +
-            "    co.nombreControl AS controlOrden,\n" +
-            "    u.nombre,\n" +
-            "    u.apellidoPaterno\n" +
-            "FROM usuario u\n" +
-            "JOIN carritocompra c ON u.idUsuario = c.idUsuario\n" +
-            "JOIN orden o ON c.idCarritoCompra = o.idCarritoCompra\n" +
-            "JOIN producto_has_carritocompra phc ON c.idCarritoCompra = phc.idCarritoCompra\n" +
-            "JOIN producto p ON phc.idProducto = p.idProducto\n" +
-            "JOIN estadoorden eo ON o.idEstadoOrden = eo.idEstadoOrden\n" +
-            "JOIN controlorden co ON o.idControlOrden = co.idControlOrden\n" +
-            "WHERE u.idUsuario = ?1\n" +
+            "    co.nombreControl AS controlOrden, \n" +
+            "    u.nombre, \n" +
+            "    u.apellidoPaterno \n" +
+            "FROM usuario u \n" +
+            "JOIN carritocompra c ON u.idUsuario = c.idUsuario \n" +
+            "JOIN orden o ON c.idCarritoCompra = o.idCarritoCompra \n" +
+            "JOIN producto_has_carritocompra phc ON c.idCarritoCompra = phc.idCarritoCompra \n" +
+            "JOIN producto p ON phc.idProducto = p.idProducto \n" +
+            "JOIN estadoorden eo ON o.idEstadoOrden = eo.idEstadoOrden \n" +
+            "JOIN controlorden co ON o.idControlOrden = co.idControlOrden \n" +
+            "WHERE u.idUsuario = :idUsuario \n" +
             "GROUP BY o.idOrden;")
-    Page <OrdenCarritoDto> obtenerCarritoConDto(Integer idUsuario, Pageable pageable);
+    Page<OrdenCarritoDto> obtenerCarritoConDto(Integer idUsuario, Pageable pageable);
+
 
     @Query(value = "SELECT \n" +
             "    (SELECT fp.foto FROM fotosproducto fp WHERE fp.idProducto = p.idProducto LIMIT 1) AS primeraFotoProducto,\n" +
