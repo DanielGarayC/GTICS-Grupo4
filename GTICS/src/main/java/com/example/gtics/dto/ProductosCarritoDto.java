4a9f56c88474b2c16b5af8aa34197f2a2c8faa20
AdrianLopez012 <142950326+AdrianLopez012@users.noreply.github.com>
2024-10-07 02:24:26 -0500

    listar carrito por asignar

---
+++ GTICS/src/main/java/com/example/gtics/dto/ProductosCarritoDto.java
@@ -0,0 +1,9 @@
+package com.example.gtics.dto;
+
+public interface ProductosCarritoDto {
+    Integer getIdProducto();
+    String getNombreProducto();
+    Integer getCantidadProducto();
+    Double getPrecioUnidad();
+    Double getPrecioTotalPorProducto();
+}
