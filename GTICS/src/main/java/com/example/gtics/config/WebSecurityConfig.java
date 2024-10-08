6b3349e26bf57b73c6c5ae062bdb9094be88ac73
RCT4H3 <139926077+RCT4H3@users.noreply.github.com>
2024-10-07 20:37:27 -0500

    Login primera parte

---
+++ GTICS/src/main/java/com/example/gtics/config/WebSecurityConfig.java
@@ -0,0 +1,56 @@
+package com.example.gtics.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig {
+    final DataSource dataSource;
+
+    public WebSecurityConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public UserDetailsManager user(DataSource dataSource) {
+        JdbcUserDetailsManager users = new JdbcUserDetailsManager(dataSource);
+        String sql1 = "select email, contrasena, activo from usuario where email=?";
+        String sql2 = "select u.email, r.nombreRol from usuario u INNER JOIN rol r ON (u.idRol = r.idRol) where email=?"
+                + "WHERE u.email = ? and u.activo = 1";
+
+        users.setUsersByUsernameQuery(sql1);
+        users.setAuthoritiesByUsernameQuery(sql2);
+        return users;
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests((authz) -> authz
+                        .anyRequest().authenticated()
+                )
+                .formLogin((form) -> form
+                        .loginPage("/Login")
+                        .loginProcessingUrl("/processLogin")
+                        .permitAll()
+                )
+                .logout(LogoutConfigurer::permitAll);
+        return http.build();
+    }
+
+}
