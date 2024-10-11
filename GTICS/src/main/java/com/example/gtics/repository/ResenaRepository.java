341d22d325eb11660a6606b9e49292d77c795654
AdrianLopez012 <142950326+AdrianLopez012@users.noreply.github.com>
2024-10-10 19:13:26 -0500

    Update ResenaRepository.java

--- GTICS/src/main/java/com/example/gtics/repository/ResenaRepository.java
+++ GTICS/src/main/java/com/example/gtics/repository/ResenaRepository.java
@@ -1,9 +1,35 @@
 package com.example.gtics.repository;
 
 import com.example.gtics.entity.Resena;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
+
 @Repository
-public interface ResenaRepository extends JpaRepository<Resena,Integer> {
+public interface ResenaRepository extends JpaRepository<Resena, Integer> {
+
+    @Query("SELECT r FROM Resena r JOIN r.producto p JOIN p.idProveedor prov " +
+            "WHERE " +
+            "(:searchKeyword IS NULL OR " +
+            "(:searchCriteria = 'name' AND p.nombreProducto LIKE CONCAT('%', :searchKeyword, '%')) OR " +
+            "(:searchCriteria = 'provider' AND prov.nombreProveedor LIKE CONCAT('%', :searchKeyword, '%'))) " +
+            "AND (:rating IS NULL OR r.idCalidad.id = :rating) " +  // Asegurar que se compara el ID correctamente
+            "AND (:startDate IS NULL OR r.fechaCreacion >= :startDate) " +
+            "AND (:endDate IS NULL OR r.fechaCreacion <= :endDate)")
+    Page<Resena> findByFilters(
+            @Param("searchCriteria") String searchCriteria,
+            @Param("searchKeyword") String searchKeyword,
+            @Param("rating") Integer rating,
+            @Param("startDate") LocalDate startDate,
+            @Param("endDate") LocalDate endDate,
+            Pageable pageable
+    );
 }
+
+
+
