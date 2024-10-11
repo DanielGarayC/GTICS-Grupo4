116932bb46c0a7dc1da6863cd59e973a4d582a48
AdrianLopez012 <142950326+AdrianLopez012@users.noreply.github.com>
2024-10-10 19:12:46 -0500

    Update FotosProductoRepository.java

--- GTICS/src/main/java/com/example/gtics/repository/FotosProductoRepository.java
+++ GTICS/src/main/java/com/example/gtics/repository/FotosProductoRepository.java
@@ -4,6 +4,7 @@ import com.example.gtics.entity.Fotosproducto;
 import com.example.gtics.entity.Producto;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
