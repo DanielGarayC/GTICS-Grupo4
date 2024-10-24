8331e7f6fdd807fffdb19d657bd3777d48ca7381
RCT4H3 <139926077+RCT4H3@users.noreply.github.com>
2024-10-23 19:19:37 -0500

    Crear nuevo usuario + validaciones a medio hacer aún

--- GTICS/src/main/java/com/example/gtics/controller/SistemaController.java
+++ GTICS/src/main/java/com/example/gtics/controller/SistemaController.java
@@ -1,18 +1,59 @@
 package com.example.gtics.controller;
 
+import com.example.gtics.ValidationGroup.RegistroUsuarioValidationGroup;
+import com.example.gtics.entity.Distrito;
+import com.example.gtics.entity.Rol;
+import com.example.gtics.entity.Usuario;
+import com.example.gtics.entity.Zona;
+import com.example.gtics.repository.DistritoRepository;
+import com.example.gtics.repository.RolRepository;
+import com.example.gtics.repository.UsuarioRepository;
+import com.example.gtics.repository.ZonaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.*;
 
 @Controller
 public class SistemaController {
-    @GetMapping({"/ExpressDealsLogin"})
+    final DistritoRepository distritoRepository;
+    final UsuarioRepository usuarioRepository;
+    final RolRepository rolRepository;
+    final ZonaRepository zonaRepository;
+
+    @Autowired
+    private BCryptPasswordEncoder passwordEncoder;
+
+    public SistemaController(DistritoRepository distritoRepository, UsuarioRepository usuarioRepository, RolRepository rolRepository, ZonaRepository zonaRepository) {
+        this.distritoRepository = distritoRepository;
+        this.usuarioRepository = usuarioRepository;
+        this.rolRepository = rolRepository;
+        this.zonaRepository = zonaRepository;
+    }
+
+    @GetMapping({"/ExpressDealsLogin","/"})
     public String Login(){
 
         return "Sistema/login";
     }
     @GetMapping({"Registro"})
-    public String Registro(){
-
+    public String Registro(Model model){
+        Usuario usuario = new Usuario();
+        List<Distrito> distritos = distritoRepository.findAll();
+        distritos.sort(new Comparator<Distrito>() {
+            @Override
+            public int compare(Distrito d1, Distrito d2) {
+                return d1.getNombre().compareTo(d2.getNombre());
+            }
+        });
+        model.addAttribute("distritos", distritos);
+        model.addAttribute("usuario", usuario);
         return "Sistema/register";
     }
     @GetMapping({"RecuperarContra"})
@@ -30,4 +71,45 @@ public class SistemaController {
 
         return "Sistema/chPass";
     }
+
+    @PostMapping("/Registrar")
+    public String registrarUsuario(Model model, @Validated(RegistroUsuarioValidationGroup.class) Usuario usuario, BindingResult bindingResult){
+        if(bindingResult.hasErrors()){
+            model.addAttribute("usuario", usuario);
+            List<Distrito> distritos = distritoRepository.findAll();
+            distritos.sort(new Comparator<Distrito>() {
+                @Override
+                public int compare(Distrito d1, Distrito d2) {
+                    return d1.getNombre().compareTo(d2.getNombre());
+                }
+            });
+            model.addAttribute("distritos", distritos);
+            return "Sistema/register";
+        }
+        Rol rol = new Rol();
+        rol.setId(4);
+        rol.setNombreRol("Usuario Final");
+        usuario.setRol(rol);
+        usuario.setBaneado(false);
+        usuario.setActivo(1);
+        List<Zona> zonas = zonaRepository.findAll();
+        List<Distrito> distritos = distritoRepository.findAll();
+        for (Distrito distrito : distritos){
+            if (Objects.equals(distrito.getId(), usuario.getDistrito().getId())){
+                for (Zona zona : zonas){
+                    if (Objects.equals(zona.getId(), distrito.getZona().getId())){
+                        usuario.setZona(zona);
+                    }
+                }
+            }
+        }
+        String encryptedPassword = passwordEncoder.encode(usuario.getContrasena());
+        usuario.setContrasena(encryptedPassword);
+
+        usuarioRepository.save(usuario);
+
+
+        return "redirect:/ExpressDealsLogin";
+    }
+
 }
--- GTICS/src/main/java/com/example/gtics/entity/Usuario.java
+++ GTICS/src/main/java/com/example/gtics/entity/Usuario.java
@@ -1,5 +1,6 @@
 package com.example.gtics.entity;
 
+import com.example.gtics.ValidationGroup.RegistroUsuarioValidationGroup;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.*;
 import lombok.Getter;
@@ -20,22 +21,24 @@ public class Usuario {
     private Integer id;
 
     @Column(name = "nombre", nullable = false, length = 45)
-    @NotBlank(message = "Debe ingresar un nombre", groups = {AdminZonalValidationGroup.class, AgenteValidationGroup.class, UsuarioFinalValidationGroup.class})
-    @Size(max = 40, message = "El nombre no puede tener más de 40 caracteres", groups = {AdminZonalValidationGroup.class, AgenteValidationGroup.class, UsuarioFinalValidationGroup.class})
+    @NotBlank(message = "Debe ingresar un nombre", groups = {AdminZonalValidationGroup.class, AgenteValidationGroup.class, UsuarioFinalValidationGroup.class, RegistroUsuarioValidationGroup.class})
+    @Size(max = 40, message = "El nombre no puede tener más de 40 caracteres", groups = {AdminZonalValidationGroup.class, AgenteValidationGroup.class, UsuarioFinalValidationGroup.class, RegistroUsuarioValidationGroup.class})
+    @Pattern(regexp = "^[a-zA-Z0-9áéíóúÁÉÍÓÚñÑ ]*$", message = "El nombre solo puede contener letras, números y espacios", groups = {AdminZonalValidationGroup.class, AgenteValidationGroup.class, UsuarioFinalValidationGroup.class, RegistroUsuarioValidationGroup.class})
     private String nombre;
 
 
     @Column(name = "email", nullable = false, length = 45)
-    @Email(message = "Debe ingresar un correo electrónico válido", groups = {AdminZonalValidationGroup.class, AgenteValidationGroup.class, UsuarioFinalValidationGroup.class})
-    @NotBlank(message = "Debe ingresar un correo electrónico", groups = {AdminZonalValidationGroup.class, AgenteValidationGroup.class, UsuarioFinalValidationGroup.class})
+    @Email(message = "Debe ingresar un correo electrónico válido", groups = {AdminZonalValidationGroup.class, AgenteValidationGroup.class, UsuarioFinalValidationGroup.class, RegistroUsuarioValidationGroup.class})
+    @NotBlank(message = "Debe ingresar un correo electrónico", groups = {AdminZonalValidationGroup.class, AgenteValidationGroup.class, UsuarioFinalValidationGroup.class, RegistroUsuarioValidationGroup.class})
     private String email;
 
     @Column(name = "direccion", nullable = false, length = 45)
-    @NotBlank(message = "Debe ingresar una dirección", groups = {AdminZonalValidationGroup.class, AgenteValidationGroup.class, UsuarioFinalValidationGroup.class})
+    @NotBlank(message = "Debe ingresar una dirección", groups = {AdminZonalValidationGroup.class, AgenteValidationGroup.class, UsuarioFinalValidationGroup.class, RegistroUsuarioValidationGroup.class})
     private String direccion;
 
     @ManyToOne(fetch = FetchType.LAZY, optional = false)
     @JoinColumn(name = "iddistrito")
+    @NotNull(message = "Debe seleccionar un distrito", groups = {AdminZonalValidationGroup.class, AgenteValidationGroup.class, UsuarioFinalValidationGroup.class, RegistroUsuarioValidationGroup.class})
     private Distrito distrito;
 
     @ManyToOne(fetch = FetchType.LAZY, optional = false)
@@ -84,15 +87,15 @@ public class Usuario {
 
 
     @Column(name = "telefono", nullable = false, length = 45)
-    @Size(min = 9, max = 9, message = "El teléfono debe tener exactamente 9 dígitos", groups = {AdminZonalValidationGroup.class, AgenteValidationGroup.class, UsuarioFinalValidationGroup.class})
-    @NotBlank(message = "Debe ingresar un número de teléfono", groups = {AdminZonalValidationGroup.class, AgenteValidationGroup.class})
-    @Pattern(regexp = "\\d+", message = "El teléfono debe contener solo dígitos", groups = {AdminZonalValidationGroup.class, AgenteValidationGroup.class})
+    @Size(min = 9, max = 9, message = "El teléfono debe tener exactamente 9 dígitos", groups = {AdminZonalValidationGroup.class, AgenteValidationGroup.class, UsuarioFinalValidationGroup.class, RegistroUsuarioValidationGroup.class})
+    @NotBlank(message = "Debe ingresar un número de teléfono", groups = {AdminZonalValidationGroup.class, AgenteValidationGroup.class, RegistroUsuarioValidationGroup.class})
+    @Pattern(regexp = "\\d+", message = "El teléfono debe contener solo dígitos", groups = {AdminZonalValidationGroup.class, AgenteValidationGroup.class, RegistroUsuarioValidationGroup.class})
     private String telefono;
 
     @Column(name = "dni", nullable = false, length = 45)
-    @Size(min = 8, max = 8, message = "El dni debe tener exactamente 8 dígitos", groups = {AdminZonalValidationGroup.class, AgenteValidationGroup.class, UsuarioFinalValidationGroup.class})
-    @NotBlank(message = "Debe ingresar un número de dni", groups = {AdminZonalValidationGroup.class, AgenteValidationGroup.class, UsuarioFinalValidationGroup.class})
-    @Pattern(regexp = "\\d+", message = "El dni debe contener solo dígitos", groups = {AdminZonalValidationGroup.class, AgenteValidationGroup.class, UsuarioFinalValidationGroup.class})
+    @Size(min = 8, max = 8, message = "El dni debe tener exactamente 8 dígitos", groups = {AdminZonalValidationGroup.class, AgenteValidationGroup.class, UsuarioFinalValidationGroup.class, RegistroUsuarioValidationGroup.class})
+    @NotBlank(message = "Debe ingresar un número de dni", groups = {AdminZonalValidationGroup.class, AgenteValidationGroup.class, UsuarioFinalValidationGroup.class, RegistroUsuarioValidationGroup.class})
+    @Pattern(regexp = "\\d+", message = "El dni debe contener solo dígitos", groups = {AdminZonalValidationGroup.class, AgenteValidationGroup.class, UsuarioFinalValidationGroup.class, RegistroUsuarioValidationGroup.class})
     private String dni;
 
     @ManyToOne(fetch = FetchType.LAZY, optional = false)
@@ -100,15 +103,18 @@ public class Usuario {
     private Zona zona;
 
     @Column(name = "apellidopaterno", nullable = false, length = 45)
-    @NotBlank(message = "Debe ingresar un apellido paterno", groups = {AdminZonalValidationGroup.class, AgenteValidationGroup.class, UsuarioFinalValidationGroup.class})
-    @Size(max = 20, message = "El apellido paterno no puede tener más de 20 caracteres", groups = {AdminZonalValidationGroup.class, AgenteValidationGroup.class, UsuarioFinalValidationGroup.class})
+    @NotBlank(message = "Debe ingresar un apellido paterno", groups = {AdminZonalValidationGroup.class, AgenteValidationGroup.class, UsuarioFinalValidationGroup.class, RegistroUsuarioValidationGroup.class})
+    @Size(max = 20, message = "El apellido paterno no puede tener más de 20 caracteres", groups = {AdminZonalValidationGroup.class, AgenteValidationGroup.class, UsuarioFinalValidationGroup.class, RegistroUsuarioValidationGroup.class})
+    @Pattern(regexp = "^[a-zA-Z0-9áéíóúÁÉÍÓÚñÑ ]*$", message = "El apellido paterno solo puede contener letras, números y espacios", groups = {AdminZonalValidationGroup.class, AgenteValidationGroup.class, UsuarioFinalValidationGroup.class, RegistroUsuarioValidationGroup.class})
     private String apellidoPaterno;
 
     @Column(name = "apellidomaterno", nullable = false, length = 45)
-    @NotBlank(message = "Debe ingresar un apellido materno", groups = {AdminZonalValidationGroup.class, AgenteValidationGroup.class, UsuarioFinalValidationGroup.class})
-    @Size(max = 20, message = "El apellido materno no puede tener más de 20 caracteres", groups = {AdminZonalValidationGroup.class, AgenteValidationGroup.class, UsuarioFinalValidationGroup.class})
+    @NotBlank(message = "Debe ingresar un apellido materno", groups = {AdminZonalValidationGroup.class, AgenteValidationGroup.class, UsuarioFinalValidationGroup.class, RegistroUsuarioValidationGroup.class})
+    @Size(max = 20, message = "El apellido materno no puede tener más de 20 caracteres", groups = {AdminZonalValidationGroup.class, AgenteValidationGroup.class, UsuarioFinalValidationGroup.class, RegistroUsuarioValidationGroup.class})
+    @Pattern(regexp = "^[a-zA-Z0-9áéíóúÁÉÍÓÚñÑ ]*$", message = "El apellido materno solo puede contener letras, números y espacios", groups = {AdminZonalValidationGroup.class, AgenteValidationGroup.class, UsuarioFinalValidationGroup.class, RegistroUsuarioValidationGroup.class})
     private String apellidoMaterno;
 
+    @NotBlank(message = "Debe ingresar una contraseña", groups = {AdminZonalValidationGroup.class, AgenteValidationGroup.class, UsuarioFinalValidationGroup.class, RegistroUsuarioValidationGroup.class})
     @Column(name = "contrasena", nullable = false, length = 45)
     private String contrasena;
 
