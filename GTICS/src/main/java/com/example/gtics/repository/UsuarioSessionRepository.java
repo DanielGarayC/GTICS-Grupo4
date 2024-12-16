adc1df460528009f00d61e0d104bac0da8885775
RCT4H3 <139926077+RCT4H3@users.noreply.github.com>
2024-12-15 22:22:45 -0500

    Algunos cambios

--- GTICS/src/main/java/com/example/gtics/repository/UsuarioSessionRepository.java
+++ GTICS/src/main/java/com/example/gtics/repository/UsuarioSessionRepository.java
@@ -1,11 +1,15 @@
 package com.example.gtics.repository;
 
+import com.example.gtics.entity.Usuario;
 import com.example.gtics.entity.UsuarioSession;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface UsuarioSessionRepository extends JpaRepository<UsuarioSession, Integer> {
 
     public UsuarioSession findByEmail(String email);
+    @Query(value = "SELECT * FROM usuario WHERE email = ? OR AGT_codigoAduana = ?;", nativeQuery = true)
+    Usuario findByEmailOrCodigoAduana(String identifier1, String identifier2);
 
 
 }
